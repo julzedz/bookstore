65d7a5adb2f7b7786381207be714ef97239f7403
Edozie <edoziechukwuma@gmail.com>
2022-11-05 16:24:11 -0600

    Add React Routers

---
+++ src/pages/Bookspage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Bookspage() {
+  return (
+    <h2>hi</h2>
+  );
+}
+
+export default Bookspage;
---
+++ src/pages/Categories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <h2>Hello</h2>
+  );
+}
+
+export default Categories;
